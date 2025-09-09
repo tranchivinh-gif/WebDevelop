44bb31c2c961cc8e14089d983bd00e2c7d7031b8
Tran Chi Vinh <vinhngungoc69@gmail.com>
2025-09-09 20:24:36 +0700

    upload result of web exercise first times

---
+++ JS/baithuchanhtrenloptuan5.js
@@ -0,0 +1,15 @@
+
+var diem = eval(prompt("nhap diem:"));
+
+switch (diem) {
+    case 10: alert("xuat sac"); break;
+    case 9: alert("gioi"); break;
+    case 8: alert("trung binh"); break;
+    case 7: alert("yeu"); break;
+    default: alert("rot mon!!!!");
+}
+
+var so = eval(prompt("nhap so can tinh bang cuu chuong:"));
+for (i = 1; i <= 10; i++) {
+    document.write(so + " * " + i + " = " + (so * i) + "<br >");
+} 
